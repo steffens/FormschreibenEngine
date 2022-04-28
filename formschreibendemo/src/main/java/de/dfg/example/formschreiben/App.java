3a5f048856ff585218a141a1968491f64aeeac09
Steffen <stessex-q@yahoo.com>
2022-04-28 19:00:59 +0200

    xdocreport added

---
+++ formschreibendemo/src/main/java/de/dfg/example/formschreiben/App.java
@@ -0,0 +1,13 @@
+package de.dfg.example.formschreiben;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
