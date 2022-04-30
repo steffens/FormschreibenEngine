c79c25b4d7cbb280d64cbf8c8596af61aad1e6a9
Steffen <stessex-q@yahoo.com>
2022-04-30 21:34:01 +0200

    works

--- formschreibendemo/src/main/java/de/dfg/example/formschreiben/App.java
+++ formschreibendemo/src/main/java/de/dfg/example/formschreiben/App.java
@@ -1,23 +1,46 @@
 package de.dfg.example.formschreiben;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+
+
+import fr.opensagres.xdocreport.core.XDocReportException;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
+
+import java.io.FileInputStream;
 import java.io.File;
 
-
 /**
- * Hello world!
+ * Super Basic Beispiel
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    throws FileNotFoundException
-    {
-        System.out.println( "Hello World!" );
-        // klären InputStream in = new FileInputStream(new File(""));
-        InputStream in = new FileInputStream(new File(""));
+public class App{
 
+    public static void main(String[] args) throws XDocReportException {
+        try (
+            
+            InputStream in = new FileInputStream(new File("Textbausteine_Korrespondenz_mit_Gutachtenden.docx"))) {
+            IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Velocity);
+            
+            IContext context = report.createContext();
+
+            System.out.println("Name: ");
+            String n = System.console().readLine();
+            if (n.endsWith("a")) {
+                context.put("anrede", "geehrte");
+            } else{
+                context.put("anrede", "geehrter");                
+            }
+            context.put("name", n);
+
+            java.io.OutputStream out = new java.io.FileOutputStream(new File("out.docx"));
+            report.process(context, out);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
